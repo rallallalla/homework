ce172bd0ebd6c5d6af4fbd4181a87f109cb0ac58
김보라 <52588809+rallallalla@users.noreply.github.com>
2020-10-15 22:19:53 +0900

    homework_sweet_hot

--- week03_homework_sweet.py
+++ week03_homework_sweet.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -14,3 +18,9 @@ for tr in trs:
     title = tr.select_one('a.title.ellipsis').text.strip()
     artist = tr.select_one('a.artist.ellipsis').text
     print(rank, title, artist)
+    doc = {
+        'rank': rank,
+        'title': title,
+        'artist': artist  # DB에는 숫자처럼 생긴 문자열 형태로 저장됩니다.
+    }
+    #db.trs.insert_one(doc)
